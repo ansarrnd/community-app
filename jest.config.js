0dfb528e1bab348cf4e62cfcba7452cd7531115e
deen <deen@mail.com>
2026-08-09 15:49:57 +0530

    feat: add full code coverage report generation and configure Jest coverage output

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,13 @@ module.exports = {
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/.expo/'],
+  collectCoverageFrom: [
+    'domain/**/*.{ts,tsx}',
+    'infrastructure/**/*.{ts,tsx}',
+    'application/**/*.{ts,tsx}',
+    'utils/**/*.{ts,tsx}',
+    'context/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+  ],
 };
